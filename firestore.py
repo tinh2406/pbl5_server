05cb3b1ed11585e1df22458682dd064b8d3a4c1e
tinh2406 <nguyenquoctinh24062002@gmail.com>
2023-03-22 15:19:32 +0700

    init server

---
+++ firestore.py
@@ -0,0 +1,103 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import random
+import json
+import datetime
+# Use a service account.
+cred = credentials.Certificate('./serviceAccount.json')
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+def addUserOwner(phone,name,password,addressDoor,addressBluetooth):
+   doc_ref = db.collection('users').document(phone)
+   doc_ref.set({
+   'name': name,
+   'password': password,
+   'addressDoor': addressDoor,
+   'addressBluetooth':addressBluetooth,
+   'owner':True
+})
+
+def addUser(phone,name,phoneOwner):
+   users_ref = db.collection('users').document(phone)
+   doc = users_ref.get().to_dict()
+   if doc!=None:
+      time = datetime.datetime.now() + datetime.timedelta(minutes=10,hours=-7)
+      doc_ref = db.collection('verifys').document(phone)
+      doc_ref.set({"code":random.randint(10000,99999),"expireAt":time})
+      return "exists account"
+
+   users_ref = db.collection('users').document(phoneOwner)
+   doc = users_ref.get().to_dict()
+   if doc == None:
+      return "no have owner"
+   
+   addressDoor = doc["addressDoor"]
+   addressBluetooth = doc["addressBluetooth"]
+
+   password = random.randint(10000,99999)
+
+   doc_ref = db.collection('users').document(phone)
+   doc_ref.set({
+   'name': name,
+   'password': password,
+   'addressDoor': addressDoor,
+   'addressBluetooth':addressBluetooth,
+   'owner':False})
+   return password
+
+
+
+def addUserExists(phone,name,phoneOwner,verification):
+   users_ref = db.collection('verifys').document(phone)
+   doc = users_ref.get().to_dict()
+   if doc==None:
+      return "error"
+
+   if str(doc["code"])!=verification or datetime.datetime.fromtimestamp(doc["expireAt"].timestamp())<datetime.datetime.now():
+      return "verification code not match"
+   
+   users_ref = db.collection('users').document(phoneOwner)
+   doc = users_ref.get().to_dict()
+   if doc == None:
+      return "no have owner"
+   
+   addressDoor = doc["addressDoor"]
+   addressBluetooth = doc["addressBluetooth"]
+
+   password = random.randint(10000,99999)
+   db.collection('verifys').document(phone).delete()
+   doc_ref = db.collection('users').document(phone)
+   doc_ref.set({
+   'name': name,
+   'password': password,
+   'addressDoor': addressDoor,
+   'addressBluetooth':addressBluetooth,
+   'owner':False})
+   return password
+
+def resetVerifyCode(phone):
+   users_ref = db.collection('users').document(phone)
+   doc = users_ref.get().to_dict()
+   if doc==None:
+      return False
+   time = datetime.datetime.now() + datetime.timedelta(minutes=10,hours=-7)
+   doc_ref = db.collection('verifys').document(phone)
+   doc_ref.set({"code":random.randint(10000,99999),"expireAt":time})
+   return True
+
+
+def updatePassword(phone,password,newPassword):
+   users_ref = db.collection('users').document(phone)
+   doc = users_ref.get().to_dict()
+   if doc == None or doc["password"]!=password:
+      return False
+   doc["password"] = newPassword
+
+   users_ref.set(doc)
+   return True
+
---
+++ index.py
@@ -0,0 +1,100 @@
+from flask import Flask, request, jsonify
+import os
+import base64
+import cv2
+import io
+import numpy as np
+from PIL import Image
+from sqlite import insert,getNameFaceWithPhone,getNamePhonewithId,deleteFaceWithId
+from trainData import train
+from detectFaces import getFace
+from firestore import updatePassword,addUser,addUserExists,resetVerifyCode
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+def home():
+    return "You can post to /api/upload."
+
+@app.route("/users/updatePassword",methods=["POST"])
+def updatePasswordAPI():
+    data = request.get_json()
+    if updatePassword(data["phone"],data["password"],data["newPassword"]):
+        return jsonify({"message":"Update password successfully"})
+    return jsonify({"message":"Password unchanged"}),403
+    
+
+@app.route("/users/addUser",methods=["POST"])
+def addUserAPI():
+    data = request.get_json()
+    res = addUser(data["phone"],data["name"],data["phoneOwner"])
+    if res == "exists account" or res == "no have owner":
+        return jsonify({"message":res})
+    return jsonify({"message":"Add account successfully"})
+
+@app.route("/users/addUserExists",methods=["POST"])
+def addUserExistsAPI():
+    data = request.get_json()
+    res = addUserExists(data["phone"],data["name"],data["phoneOwner"],data["verification"])
+    if res=="error" or res == "verification code not match" or res == "no have owner":
+        return jsonify({"message":res})
+    return jsonify({"message":"Add account successfully"})
+@app.route("/users/resendVerifyCode",methods=["POST"])
+def resendVerifyCode():
+    data = request.get_json()
+    res = resetVerifyCode(data["phone"])
+    if res:
+        return jsonify({"message":"Resend success"})
+    return jsonify({"message":"Resend error"})
+
+@app.route("/users/addUserOwner",methods=["POST"])
+def addUserOwner():
+    data = request.get_json()
+    
+    return "abc"
+
+@app.route("/faces/<id>",methods=["DELETE"])
+def deleteFaceAPI(id):
+    faces = getNamePhonewithId(id)
+    if len(faces)==0:
+        return jsonify({"error":"face not found"}),404
+    face = faces[0]
+    deleteFaceWithId(id)
+    path = "./dataSet/"+face["phone"]+"/"+face["name"]
+    for root,dirs,files in os.walk(path):
+        for file in files:
+            os.remove(os.path.join(root,file))
+    try:
+        os.rmdir(path)
+    except:
+        print("")
+    train()
+    return jsonify({"message":"face deleted successfully"}),200
+
+@app.route("/faces/<phone>")
+def getFacesAPI(phone):
+    return jsonify({"data":getNameFaceWithPhone(phone)})
+
+@app.route("/api/upload", methods=["POST"])
+def upload():
+    data = request.get_json()
+    phone = data["phone"]
+    name = data["name"]
+    count = int(data["count"])
+    image = data["image"]
+    
+    npImage = np.array(Image.open(io.BytesIO(base64.b64decode(image))),'uint8')
+
+    if getFace(cv2.flip(cv2.rotate(npImage, cv2.ROTATE_90_COUNTERCLOCKWISE),1),phone,name,count):
+        if count >= 5:
+            insert(name,phone)
+            train()
+            return jsonify({"message": "success"})
+        else:
+            return jsonify({"message": "need further data"})
+    return jsonify({"message":"khong co mat"})
+
+if __name__ == "__main__":
+    app.run(debug=True, host="192.168.43.98", port=os.environ.get("PORT", 3000))
