7baa0c76bdd21d1bf857e26f0dbdf21625452b69
= <=>
2023-05-05 17:28:37 +0700

    duy commit

--- face_recognition.py
+++ face_recognition.py
@@ -70,9 +70,6 @@ def recognize_faces(ipESP32):
         font = cv2.FONT_HERSHEY_SIMPLEX
         #iniciate id counter
         id = 0
-        # names related to ids: example ==> Marcelo: id=1,  etc
-        # names = ['None','Duy Nguyen','Obama','Donald Trump'] 
-        # Initialize and start realtime video capture
         cam = cv2.VideoCapture(0)
         cam.set(3, 640) # set video widht
         cam.set(4, 480) # set video height
@@ -90,8 +87,13 @@ def recognize_faces(ipESP32):
         eye_privious_right = 1
         eye_privious_left = 1
 
-        while True:
-            
+        
+        # if not ret:
+        #     break
+        while True:           
+            ret, img =cam.read()
+            img = cv2.flip(img, 1) # Flip vertically
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
             if time.time() - start_time > 150:
                 print('Recognize faces timeout')
                 # cam.release()
@@ -106,20 +108,7 @@ def recognize_faces(ipESP32):
         
             # cv2.imshow('live transmission',img)
             #  =================================================================
-            ret, img =cam.read()
-            img = cv2.flip(img, 1) # Flip vertically
-            if not ret:
-                break
 
-            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            faces_eyes = detector(gray)
-            faces = faceCascade.detectMultiScale( 
-                gray,
-                scaleFactor = 1.2,
-                minNeighbors = 5,
-                minSize = (int(minW), int(minH)),
-            )
-        
             # if (cv2.getTickCount() - pre_time) / cv2.getTickFrequency() < 5:
             #     print('stop detecting')
             # # if (datetime.datetime.now() - pre_time) < 5:
@@ -127,118 +116,127 @@ def recognize_faces(ipESP32):
             #     # print('stop detecting ' + str(count))
             #     continue
             # else:
-            for face in faces_eyes:
-                shapes = predictor(gray, face)
-                shapes = face_utils.shape_to_np(shapes)
+            if blinks < 2:
+                faces_eyes = detector(gray)
+                for face in faces_eyes:
+                    shapes = predictor(gray, face)
+                    shapes = face_utils.shape_to_np(shapes)
 
-                eye_img_l, eye_rect_l = crop_eye(gray, eye_points=shapes[36:42])
-                eye_img_r, eye_rect_r = crop_eye(gray, eye_points=shapes[42:48])
+                    eye_img_l, eye_rect_l = crop_eye(gray, eye_points=shapes[36:42])
+                    eye_img_r, eye_rect_r = crop_eye(gray, eye_points=shapes[42:48])
 
-                eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
-                eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
-                eye_img_r = cv2.flip(eye_img_r, flipCode=1)
+                    eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
+                    eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
+                    eye_img_r = cv2.flip(eye_img_r, flipCode=1)
 
-                eye_input_l = eye_img_l.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
-                eye_input_r = eye_img_r.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
+                    eye_input_l = eye_img_l.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
+                    eye_input_r = eye_img_r.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
 
-                pred_l = model.predict(eye_input_l)
-                pred_r = model.predict(eye_input_r)
+                    pred_l = model.predict(eye_input_l)
+                    pred_r = model.predict(eye_input_r)
 
-                # visualize
-                state_l = '%.1f' if pred_l > 0.1 else '%.1f'
-                state_r = '%.1f' if pred_r > 0.1 else '%.1f'
+                    # visualize
+                    state_l = '%.1f' if pred_l > 0.1 else '%.1f'
+                    state_r = '%.1f' if pred_r > 0.1 else '%.1f'
 
-                state_l = state_l % pred_l
-                state_r = state_r % pred_r
-                color_l = (0,255,0)
-                color_r = ()
-                if float(state_l) > 0.1 :
+                    state_l = state_l % pred_l
+                    state_r = state_r % pred_r
                     color_l = (0,255,0)
-                else : 
-                    color_l = (0,0,255)
-                if float(state_r) > 0.1 :
-                    color_r = (0,255,0)
-                else : 
-                    color_r = (0,0,255)
+                    color_r = ()
+                    if float(state_l) > 0.1 :
+                        color_l = (0,255,0)
+                    else : 
+                        color_l = (0,0,255)
+                    if float(state_r) > 0.1 :
+                        color_r = (0,255,0)
+                    else : 
+                        color_r = (0,0,255)
+                        
+                    cv2.rectangle(img, pt1=tuple(eye_rect_l[0:2]), pt2=tuple(eye_rect_l[2:4]), color=color_l, thickness=2)
+                    cv2.rectangle(img, pt1=tuple(eye_rect_r[0:2]), pt2=tuple(eye_rect_r[2:4]), color=color_r, thickness=2)
+
+                    cv2.putText(img, state_l, tuple(eye_rect_l[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color_l, 2)
+                    cv2.putText(img, state_r, tuple(eye_rect_r[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color_r, 2)
                     
-                cv2.rectangle(img, pt1=tuple(eye_rect_l[0:2]), pt2=tuple(eye_rect_l[2:4]), color=color_l, thickness=2)
-                cv2.rectangle(img, pt1=tuple(eye_rect_r[0:2]), pt2=tuple(eye_rect_r[2:4]), color=color_r, thickness=2)
-
-                cv2.putText(img, state_l, tuple(eye_rect_l[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color_l, 2)
-                cv2.putText(img, state_r, tuple(eye_rect_r[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color_r, 2)
-                
-                
-                if float(state_l) < 0.5 and float(state_r) < 0.5:
-                    # if time.time() - last_blink_time < 1:
-                    #     continue
-                    if eye_privious_right > 0.5 or eye_privious_left > 0.5:
-                        blinks += 1
-                        last_blink_time = time.time()
-                eye_privious_right = float(state_r)
-                eye_privious_left = float(state_l)
-                print('so lan nhay mat ', blinks)
-                if blinks >= 2:
-                    check = False
-                    for(x,y,w,h) in faces:
-                        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-                        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-                        # Check if confidence is less them 100 ==> "0" is perfect match 
-                        if (confidence < 85):
-                            name = getProfile(id)[1]
-                            confidence = " {0}%".format(round(100 - confidence))
-                            # print(name)
-                            # print(confidence)
-                            #pre_time = cv2.getTickCount()
-                            #pre_time = datetime.datetime.now()
-                            #data = {'data': 'open'}
-                            #response = requests.post(urlUnlock, data=data)
-                            #print(response.status_code)
-                            # count = 0
-                            # luu anh vao thu muc imagesSaved
-                            now = datetime.datetime.now()
-                            # imgTime = now.strftime("%d-%m-%y_%Hh%Mm%Ss")
-                            # imgPathRecognize = "./imagesSaved/" + imgTime +"_"+ name + ".jpg" 
-                            # addHistory('192.168.1.5',name)
-                            check = True
-                            unknown_count = 0
-                        else:
-                            name = "unknown"
-                            confidence = "  {0}%".format(round(100 - confidence))
-                            unknown_count += 1
-                            # if unknown_count >= 200:
-                            #     print("Đây là giả mạo")
-                            #     unknown_count = 0
-                        # print(str(getProfile(id)))
-                        # print(str(confidence))
-                        cv2.putText(img, name, (x+5,y-5), font, 1, (255,255,255), 2)
-                        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)
-                        if check == True:
-                            # cv2.imwrite(imgPathRecognize, img)
-                            # cam.release()
-                            # cv2.destroyAllWindows()
-                            print('successfully recognition')
-                            # return True
-                    if unknown_count >= 200:
-                        print('day la nguoi la')
+                    
+                    if float(state_l) < 0.5 and float(state_r) < 0.5:
+                        # if time.time() - last_blink_time < 1:
+                        #     continue
+                        if eye_privious_right > 0.5 or eye_privious_left > 0.5:
+                            blinks += 1
+                            last_blink_time = time.time()
+                    eye_privious_right = float(state_r)
+                    eye_privious_left = float(state_l)
+                    print('so lan nhay mat ', blinks)
+                    if blinks < 2:
+                        cv2.putText(img, 'Fake Face', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            else: 
+                faces = faceCascade.detectMultiScale( 
+                        gray,
+                        scaleFactor = 1.2,
+                        minNeighbors = 5,
+                        minSize = (int(minW), int(minH)),
+                    )
+                check = False
+                for(x,y,w,h) in faces:
+                    cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+                    id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+                    # Check if confidence is less them 100 ==> "0" is perfect match 
+                    if (confidence < 85):
+                        name = getProfile(id)[1]
+                        confidence = " {0}%".format(round(100 - confidence))
+                        # print(name)
+                        # print(confidence)
+                        #pre_time = cv2.getTickCount()
+                        #pre_time = datetime.datetime.now()
+                        #data = {'data': 'open'}
+                        #response = requests.post(urlUnlock, data=data)
+                        #print(response.status_code)
+                        # count = 0
+                        # luu anh vao thu muc imagesSaved
                         now = datetime.datetime.now()
-                        imgTimeUnknown = now.strftime("%d-%m-%y_%Hh%Mm%Ss")
-                        imgPathUnknown = "./unknownImage/" + imgTimeUnknown +"_" + "unknown" + ".jpg" 
-                        cv2.imwrite(imgPathUnknown,img)
-                        # nameUnknown = "Unknown_" + imgTimeUnknown + ".jpg"
-
-                        # urlImage = addImageToStorage('notify',imgPathUnknown,nameUnknown)
-                        # addNotify('192.168.1.5','Co nguoi la',urlImage)
+                        # imgTime = now.strftime("%d-%m-%y_%Hh%Mm%Ss")
+                        # imgPathRecognize = "./imagesSaved/" + imgTime +"_"+ name + ".jpg" 
+                        # addHistory('192.168.1.5',name)
+                        check = True
                         unknown_count = 0
+                    else:
+                        name = "unknown"
+                        confidence = "  {0}%".format(round(100 - confidence))
+                        unknown_count += 1
+                        # if unknown_count >= 200:
+                        #     print("Đây là giả mạo")
+                        #     unknown_count = 0
+                    # print(str(getProfile(id)))
+                    # print(str(confidence))
+                    cv2.putText(img, name, (x+5,y-5), font, 1, (255,255,255), 2)
+                    cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)
+                    if check == True:
+                        # cv2.imwrite(imgPathRecognize, img)
                         # cam.release()
                         # cv2.destroyAllWindows()
-                        print('finished recognition: failed')
-                        # return False
-                else:
-                    cv2.putText(img, 'Fake Face', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                        print('successfully recognition')
+                        # return True
+                if unknown_count >= 200:
+                    print('day la nguoi la')
+                    now = datetime.datetime.now()
+                    imgTimeUnknown = now.strftime("%d-%m-%y_%Hh%Mm%Ss")
+                    imgPathUnknown = "./unknownImage/" + imgTimeUnknown +"_" + "unknown" + ".jpg" 
+                    cv2.imwrite(imgPathUnknown,img)
+                    # nameUnknown = "Unknown_" + imgTimeUnknown + ".jpg"
 
-                # reset blink count if no blinks detected in the last 5 seconds
-                # if time.time() - last_blink_time > 5:
-                #     blinks = 0
+                    # urlImage = addImageToStorage('notify',imgPathUnknown,nameUnknown)
+                    # addNotify('192.168.1.5','Co nguoi la',urlImage)
+                    unknown_count = 0
+                    # cam.release()
+                    # cv2.destroyAllWindows()
+                    print('finished recognition: failed')
+                    # return False
+                    
+
+                    # reset blink count if no blinks detected in the last 5 seconds
+                    # if time.time() - last_blink_time > 5:
+                    #     blinks = 0
             cv2.imshow('camera',img)
             k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
             if k == 27:
